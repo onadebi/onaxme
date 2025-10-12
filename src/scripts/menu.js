490494591eb2e645d76681c78ed47e9b8c6c93ee
Onaefe Edebi <onaefe@outlook.com>
2025-10-12 10:59:19 +0400

    menu animation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('#line1').classList.toggle('slanted-forward');
+    document.querySelector('#line3').classList.toggle('slanted');
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
